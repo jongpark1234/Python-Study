4636cd593533a7022bfe70d299abf6c232093380
JongPark <jongpark1234@naver.com>
2024-01-12 15:34:25 +0900

    Update

--- process-thread-sum/main.py
+++ process-thread-sum/main.py
@@ -16,35 +16,33 @@ def calculate_sum(ranges: Tuple[int]) -> int:
     return total_sum
         
 
-def do_threading(ranges: Tuple[int]) -> None:
-    start, end = ranges
+def do_threading(ranges: Tuple[int]) -> List[List[int]]:
     num_threads = 2
-    
+    start, end = ranges
     thread_ranges = get_ranges_array(start, end, num_threads)
     
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
         return list(executor.map(calculate_sum, thread_ranges))
 
 
-def do_processing() -> None:
-    start, end = 1, 100000
+def do_processing(start: int, end: int) -> None:
     num_processes = 4
-
     process_ranges = get_ranges_array(start, end, num_processes)
     
     with multiprocessing.Pool(processes=num_processes) as pool:
-        results = pool.map(do_threading, process_ranges)
-        print(sum(map(sum, results)))
+        return sum(map(sum, pool.map(do_threading, process_ranges)))
     
 
 if __name__ == "__main__":
+    start, end = 1, 100_000
     start_time = time.time()
-    do_processing()
+    print(do_processing(start, end))
     end_time = time.time()
     parallel = end_time - start_time
     
     start_time = time.time()
-    print(sum(i for i in range(1, 100001)))
+    print(sum(i for i in range(start, end + 1)))
     end_time = time.time()
     serial = end_time - start_time
+    
     print(parallel, serial)
