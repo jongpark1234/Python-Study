04b8532c1ae2e04a6b745d95ed9ec45026ac233d
JongPark <jongpark1234@naver.com>
2024-01-12 17:32:22 +0900

    multiprocessing -> concurrent.futures

--- process-thread-sum/main.py
+++ process-thread-sum/main.py
@@ -1,6 +1,5 @@
 import time
 import concurrent.futures
-import multiprocessing
 from typing import List, Tuple
 
 def get_ranges_array(start: int, end: int, divisor: int) -> List[Tuple[int]]:
@@ -14,35 +13,29 @@ def calculate_sum(ranges: Tuple[int]) -> int:
     for i in range(start, end + 1):
         total_sum += i
     return total_sum
-        
+
 
 def do_threading(ranges: Tuple[int]) -> List[List[int]]:
     num_threads = 2
     start, end = ranges
     thread_ranges = get_ranges_array(start, end, num_threads)
     
-    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-        return list(executor.map(calculate_sum, thread_ranges))
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as thread_executor:
+        return list(thread_executor.map(calculate_sum, thread_ranges))
 
 
 def do_processing(start: int, end: int) -> int:
     num_processes = 4
     process_ranges = get_ranges_array(start, end, num_processes)
     
-    with multiprocessing.Pool(processes=num_processes) as pool:
-        return sum(map(sum, pool.map(do_threading, process_ranges)))
-    
+    with concurrent.futures.ProcessPoolExecutor(max_workers=num_processes) as process_executor:
+        return sum(map(sum, process_executor.map(do_threading, process_ranges)))
+
 
 if __name__ == "__main__":
-    start, end = 1, 100_000
+    start, end = 1, 100_000_000
     start_time = time.time()
     print(do_processing(start, end))
     end_time = time.time()
     parallel = end_time - start_time
-    
-    start_time = time.time()
-    print(sum(i for i in range(start, end + 1)))
-    end_time = time.time()
-    serial = end_time - start_time
-    
-    print(parallel, serial)
+    print(parallel)
